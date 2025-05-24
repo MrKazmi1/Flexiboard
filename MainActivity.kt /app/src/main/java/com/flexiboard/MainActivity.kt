837b6859f1a1dd50619526b87ec3a12d20ab155e
MrKazmi1 <98674025+MrKazmi1@users.noreply.github.com>
2025-05-25 02:03:17 +0500

    Create MainActivity.kt

---
+++ MainActivity.kt /app/src/main/java/com/flexiboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.flexiboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // You can add settings UI here later
+    }
+}
